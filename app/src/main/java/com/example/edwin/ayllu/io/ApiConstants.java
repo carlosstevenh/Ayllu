32f0265a9864fc8e1ceab7679682c8146fc4d18b
Steven Hurtado <carlossteveh@hotmail.com>
2017-05-08 14:38:27 -0500

    Correccion URL servidor

--- app/src/main/java/com/example/edwin/ayllu/io/ApiConstants.java
+++ app/src/main/java/com/example/edwin/ayllu/io/ApiConstants.java
@@ -4,7 +4,6 @@ public class ApiConstants {
     public static final String URL_API_AYLLU = "http://138.68.40.165/api.ayllu.com/";
     public static final String URL_IMG = "http://138.68.40.165/camara/imagenes/";
 
-    //public static final String URL_WEBSERVICE = "http://138.68.40.165/webservice/";
-    public static final String URL_WEBSERVICE = "http://192.168.56.1/webservice/";
+    public static final String URL_WEBSERVICE = "http://138.68.40.165/webservice/";
     public static final String URL_CAMERA = "http://138.68.40.165/camara/";
 }
